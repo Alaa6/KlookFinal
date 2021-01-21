4add715b166b016426507441fca105e58b8257cb
OlaAwad <ola.khireldin@gmail.com>
2021-01-21 23:00:16 +0200

    Final Edit in Header & Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [
-  {path: 'Home', component:HomeComponent},
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
