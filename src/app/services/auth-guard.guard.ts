80cee8296d29273adb7a08c9f8531fb9b5ffbd9f
hagarelgarh98 <hagar.elgarh98@gmail.com>
2021-02-03 22:04:35 +0400

    go

--- src/app/services/auth-guard.guard.ts
+++ src/app/services/auth-guard.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { promises } from 'dns';
+// import { promises } from 'dns';
 // import { promise } from 'protractor';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
