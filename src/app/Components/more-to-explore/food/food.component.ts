85d1045d3b6994f8c0bf3a8cb8240daf0d1936b9
basmahesham8 <basmahesham8@gmail.com>
2021-03-13 22:18:23 +0200

    bbb

--- src/app/Components/more-to-explore/food/food.component.ts
+++ src/app/Components/more-to-explore/food/food.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { FoodServiceService } from 'src/app/services/food-service.service';
 import { LanguageServiceService } from 'src/app/services/language-service.service';
 import { IFood } from 'src/app/viewModels/ifood';
 import { PageEvent } from '@angular/material/paginator';
-
+import { IContinent } from 'src/app/viewModels/icontinent';
+import { ISubCategory } from 'src/app/viewModels/isub-category';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
+import { RelaxServiceService } from 'src/app/services/relax-service.service';
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
@@ -33,17 +45,101 @@ export class FoodComponent implements OnInit, OnDestroy {
   //   this.pageSize = event.pageSize;
   //   this.pageIndex = event.pageIndex;
   // }
+  searchkey: string = '';
+  subCatName: string = '';
+  city: string = '';
+  // toursSearch: ITour[] = []
+  // jsoon: ICategory[] = []
+  searchTerm: string = '';
+  // toursSearchdisplay: ITour[] = []
+  priceRangeValue: number;
+  priceRangeMax: number;
+  priceRangeMin: number;
+  // tourList: ITour[] = []
+  subCat: string = '';
+  // cityList: ICity[] = []
+  continentNameList: string[] = [];
+  continentName: string = '';
+  showCity: Boolean = false;
+  activitesCategory: string = '';
+  cityName: string = '';
+  activCatName: string = '';
+  collectionListName: string[] = [];
+  collectionName: string = '';
+
+  private _transformerCities = (node: IContinent, level: number) => {
+    return {
+      expandable: !!node.Cities && node.Cities.length > 0,
+      name: node.Name,
+      level: level,
+    };
+  };
+  private _transformerCategories = (node: ISubCategory, level: number) => {
+    return {
+      expandable: !!node.ActivCategories && node.ActivCategories.length > 0,
+      name: node.Name,
+      level: level,
+    };
+  };
+
+  treeControl: any = new FlatTreeControl<ExampleFlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattenerCities: any = new MatTreeFlattener(
+    this._transformerCities,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.Cities
+  );
+
+  treeFlattenerCategories: any = new MatTreeFlattener(
+    this._transformerCategories,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.ActivCategories
+  );
+  dataSourceCities: any = new MatTreeFlatDataSource(
+    this.treeControl,
+    this.treeFlattenerCities
+  );
+
+  dataSourceCatefories: any = new MatTreeFlatDataSource(
+    this.treeControl,
+    this.treeFlattenerCategories
+  );
 
   constructor(
     private languageService: LanguageServiceService,
     private translate: TranslateService,
     private foodServics: FoodServiceService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private relaxService: RelaxServiceService
   ) {
     this.translate.use(languageService.getLanguage());
   }
-
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
   ngOnInit(): void {
+    this.relaxService.getContinentList().subscribe((res) => {
+      this.dataSourceCities.data = res.map((data) => {
+        return {
+          id: data.payload.doc.id,
+          ...data.payload.doc.data(),
+        };
+      });
+    });
+
+    this.relaxService.getSubCategory().subscribe((res) => {
+      this.dataSourceCatefories.data = res.map((data) => {
+        return {
+          id: data.payload.doc.id,
+          ...data.payload.doc.data(),
+        };
+      });
+    });
+
     this.subscribtion = this.foodServics
       .getFoodCards('Categories', 'Section', 'Food', 'Other')
       .subscribe(
@@ -73,5 +169,31 @@ export class FoodComponent implements OnInit, OnDestroy {
         console.log('cant find ' + e);
       }
     );
+  }
+
+  setCityName(cityName: string) {
+    this.router.navigate([
+      '/experiences/activities/',
+      {
+        city: cityName,
+        supCatName: this.subCatName,
+        activitesCategory: this.activitesCategory,
+        collectionName: this.collectionName,
+      },
+    ]);
+  }
+
+  setActivCategoryName(activCate: string) {
+    console.log(activCate);
+
+    this.router.navigate([
+      '/experiences/activities/',
+      {
+        city: this.city,
+        supCatName: this.subCatName,
+        activitesCategory: activCate,
+        collectionName: this.collectionName,
+      },
+    ]);
   }
 }
