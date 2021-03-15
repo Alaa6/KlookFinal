efb677d0da881c2ac2411fb71fcf184841c8ca37
OlaAwad <ola.khireldin@gmail.com>
2021-03-15 04:43:11 +0200

    booking & style

--- src/app/Components/admins/sign/sign.component.ts
+++ src/app/Components/admins/sign/sign.component.ts
@@ -1,78 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 // import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-import { User } from '../../../viewModels/user';
-import { FormGroup, FormBuilder ,FormControl, Validators} from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router'
+import { AuthService } from 'src/app/services/auth.service'
+import { User } from '../../../viewModels/user'
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
+import { Subscription } from 'rxjs'
 // import { Guid } from "guid-typescript";
-
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
-  styleUrls: ['./sign.component.scss']
+  styleUrls: ['./sign.component.scss'],
 })
 export class SignComponent implements OnInit {
-  insertForm: FormGroup;
+  insertForm: FormGroup
 
-  
-  loginFrm:FormGroup;
-list:User[]=[]
-errorMsg:string=''
-loginBool:boolean = false;
-signbool:boolean = false;
-userType:string='user'
-users:User[]=[]
-Signn:string='Sign Up'
-userId: string='';
+  loginFrm: FormGroup
+  list: User[] = []
+  errorMsg: string = ''
+  loginBool: boolean = false
+  signbool: boolean = false
+  userType: string = 'user'
+  users: User[] = []
+  Signn: string = 'Sign Up'
+  userId: string = ''
 
-
-
-    Password=new FormControl('',[Validators.required,Validators.minLength(5)]);
-    Email= new FormControl('',[Validators.required,Validators.minLength(6),Validators.email,Validators.maxLength(100)]);
+  Password = new FormControl('', [Validators.required, Validators.minLength(5)])
+  Email = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+    Validators.email,
+    Validators.maxLength(100),
+  ])
 
   getErrorMessage() {
-      return 'You must enter the right value';
-    
+    return 'You must enter the right value'
   }
-  listadd:User={Email:'',Password:'',Name:''}
-  signupp(){
- 
+  listadd: User = { Email: '', Password: '', Name: '' }
+  signupp() {
     ////old////
-    this.listadd=this.insertForm.value;
+    this.listadd = this.insertForm.value
     // this.listadd.Email=this.Email.value;
     // this.listadd.Password=this.Password.value;
     // this.listadd.Name=this.Password.value;
-    this.listadd.JoinDate=Date.now()
-    
+    this.listadd.JoinDate = Date.now()
+
     console.log(this.listadd)
     console.log(this.listadd.Email)
     console.log(this.listadd.Password)
     console.log(this.listadd.Name)
-    this.authSer.signup(this.listadd)
-      .then(result => {
-        this.errorMsg='';
-          // localStorage.setItem('currentUser',result.user?.uid);
+    this.authSer
+      .signup(this.listadd)
+      .then((result) => {
+        this.errorMsg = ''
+        // localStorage.setItem('currentUser',result.user?.uid);
 
-// localStorage.setItem('currentUserName', this.listadd.Name);
-      // this.authSer.userLogin=false
-          // this.router.navigate(['/']);
-        this.authSer.addUser(result.user?.uid,this.listadd)
-        
-        .then(()=>{
-          localStorage.setItem('currentUser',result.user?.uid);
-      localStorage.setItem('currentUserName', this.listadd.Name);
-      localStorage.setItem('currentUserEmail', this.listadd.Email);
+        // localStorage.setItem('currentUserName', this.listadd.Name);
+        // this.authSer.userLogin=false
+        // this.router.navigate(['/']);
+        this.authSer
+          .addUser(result.user?.uid, this.listadd)
 
-      this.authSer.userLogin=false
-          this.router.navigate(['/']);
-        }
-        ).catch(errr=>console.log(errr))      
+          .then(() => {
+            localStorage.setItem('currentUser', result.user?.uid)
+            localStorage.setItem('currentUserName', this.listadd.Name)
+            localStorage.setItem('currentUserEmail', this.listadd.Email)
+
+            this.authSer.userLogin = false
+            // this.router.navigate(['/'])
+            window.history.back()
+
+            // window.location.reload()
+          })
+          .catch((errr) => console.log(errr))
       })
-      .catch(err => {
-        this.errorMsg=err.message
-      } )
+      .catch((err) => {
+        this.errorMsg = err.message
+      })
     ////old////
 
     ////// new ////////////
@@ -81,7 +85,7 @@ userId: string='';
     // // this.listadd.Password=this.Password.value;
     // // this.listadd.Name=this.Password.value;
     // this.listadd.JoinDate=Date.now()
-    
+
     // console.log(this.listadd)
     // console.log(this.listadd.Email)
     // console.log(this.listadd.Password)
@@ -104,150 +108,142 @@ userId: string='';
     //     console.log("errrrrorrr"+err)
     //   })
     ////// new ////////////
-
-  
-
-    
   }
-  constructor(private router:Router,private authSer: AuthService,private fb: FormBuilder,private activatedRoute: ActivatedRoute) {
-    this.loginFrm=this.fb.group({
-     
-     
-    });
-   }
+  constructor(
+    private router: Router,
+    private authSer: AuthService,
+    private fb: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.loginFrm = this.fb.group({})
+  }
 
   ngOnInit(): void {
     this.insertForm = this.fb.group({
-      Email:["", Validators.required],
-      Password:["", [Validators.required, Validators.minLength(4)]],
-      Name:["", [Validators.required, Validators.minLength(4)]]
-    });
+      Email: ['', Validators.required],
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      Name: ['', [Validators.required, Validators.minLength(4)]],
+    })
   }
-  
 
-  username:User={}
-  login(){
-
-
+  username: User = {}
+  login() {
     /////old ////
-    this.listadd=this.insertForm.value;
+    this.listadd = this.insertForm.value
 
-    this.authSer.login(this.listadd.Email,this.listadd.Password)
-    .then(result=>
-      {
-        
-        this.errorMsg='';
-        this.authSer.loginUser(this.listadd.Email,this.listadd.Password).subscribe((res) => {
-          // test@tset.com
-          console.log(res.length<1)
-          this.errorMsg='this user is no longer Exist'
+    this.authSer
+      .login(this.listadd.Email, this.listadd.Password)
+      .then((result) => {
+        this.errorMsg = ''
+        this.authSer
+          .loginUser(this.listadd.Email, this.listadd.Password)
+          .subscribe(
+            (res) => {
+              // test@tset.com
+              console.log(res.length < 1)
+              this.errorMsg = 'this user is no longer Exist'
 
-                this.list = res.map(data => {
-                  this.userId=data.payload.doc.id
-                  this.username=data.payload.doc.data()
-                  console.log(this.username.Name)
-                  this.authSer.userId=this.userId
-                localStorage.setItem('currentUser', this.userId);
-                localStorage.setItem('currentUserName', this.username.Name);
-                localStorage.setItem('currentUserEmail', this.username.Email);
+              this.list = res.map((data) => {
+                this.userId = data.payload.doc.id
+                this.username = data.payload.doc.data()
+                console.log(this.username.Name)
+                this.authSer.userId = this.userId
+                localStorage.setItem('currentUser', this.userId)
+                localStorage.setItem('currentUserName', this.username.Name)
+                localStorage.setItem('currentUserEmail', this.username.Email)
 
-                this.authSer.userLogin=false
-          
-          console.log(this.userId)
-          console.log(this.username.Name)
-          
-                  // console.log(this.userId)
-                this.router.navigate(['/']);
-                this.authSer.userLogin=false
-          
+                this.authSer.userLogin = false
+
+                console.log(this.userId)
+                console.log(this.username.Name)
+
+                // console.log(this.userId)
+                // this.router.navigate(['/'])
+                this.authSer.userLogin = false
+                window.history.back()
+
                 // this.authSer.userLogin=true
                 console.log(this.authSer.userLogin)
-          
-          
-                  return {
-                    id: data.payload.doc.id,
-                    ...data.payload.doc.data()
-                  }
-                });
-                // this.loading = false
-              }, (err) => {console.log(err)
-              
-                this.errorMsg='this user is no longer Exist'
-              
-              });
-              // console.log(this.list)
-            })
-    .catch(err=>
-      {
-        this.errorMsg=err.message
 
-      console.log(err)
-      
-      
+                return {
+                  id: data.payload.doc.id,
+                  ...data.payload.doc.data(),
+                }
+              })
+              // this.loading = false
+            },
+            (err) => {
+              console.log(err)
+
+              this.errorMsg = 'this user is no longer Exist'
+            },
+          )
+        // console.log(this.list)
       })
-      
+      .catch((err) => {
+        this.errorMsg = err.message
+
+        console.log(err)
+      })
+
     /////old ////
-//     this.listadd=this.insertForm.value;
-//     // this.listadd.Email=this.Email.value;
-//     // this.listadd.Password=this.Password.value;
-//     // this.listadd.Name=this.Password.value;
-//     this.listadd.JoinDate=Date.now()
-    
-//     console.log(this.listadd)
-//     console.log(this.listadd.Email)
-//     console.log(this.listadd.Password)
-//     console.log(this.listadd.Name)
+    //     this.listadd=this.insertForm.value;
+    //     // this.listadd.Email=this.Email.value;
+    //     // this.listadd.Password=this.Password.value;
+    //     // this.listadd.Name=this.Password.value;
+    //     this.listadd.JoinDate=Date.now()
 
-//     this.authSer.login(this.listadd.Email,this.listadd.Password).subscribe((res) => {
+    //     console.log(this.listadd)
+    //     console.log(this.listadd.Email)
+    //     console.log(this.listadd.Password)
+    //     console.log(this.listadd.Name)
 
-//       this.list = res.map(data => {
-//         this.userId=data.payload.doc.id
-//         this.username=data.payload.doc.data()
-//         this.authSer.userId=this.userId
-//       localStorage.setItem('currentUser', this.userId);
-//       localStorage.setItem('currentUserName', this.username.Name);
-//       this.authSer.userLogin=false
+    //     this.authSer.login(this.listadd.Email,this.listadd.Password).subscribe((res) => {
 
-// console.log(this.userId)
-// console.log(this.username.Name)
+    //       this.list = res.map(data => {
+    //         this.userId=data.payload.doc.id
+    //         this.username=data.payload.doc.data()
+    //         this.authSer.userId=this.userId
+    //       localStorage.setItem('currentUser', this.userId);
+    //       localStorage.setItem('currentUserName', this.username.Name);
+    //       this.authSer.userLogin=false
 
-//         // console.log(this.userId)
-//       this.router.navigate(['/']);
-//       this.authSer.userLogin=false
+    // console.log(this.userId)
+    // console.log(this.username.Name)
 
-//       // this.authSer.userLogin=true
-//       console.log(this.authSer.userLogin)
+    //         // console.log(this.userId)
+    //       this.router.navigate(['/']);
+    //       this.authSer.userLogin=false
 
+    //       // this.authSer.userLogin=true
+    //       console.log(this.authSer.userLogin)
 
-//         return {
-//           id: data.payload.doc.id,
-//           ...data.payload.doc.data()
-//         }
-//       });
-//       // this.loading = false
-//     }, (err) => console.log(err))
-//     console.log(this.list)
-    }
-      subsign: string = '';
+    //         return {
+    //           id: data.payload.doc.id,
+    //           ...data.payload.doc.data()
+    //         }
+    //       });
+    //       // this.loading = false
+    //     }, (err) => console.log(err))
+    //     console.log(this.list)
+  }
+  subsign: string = ''
 
-   routeSubscription: Subscription = this.activatedRoute.paramMap.subscribe((params: ParamMap) => {    //if  the route parameter value  changes  (Observable) 
+  routeSubscription: Subscription = this.activatedRoute.paramMap.subscribe(
+    (params: ParamMap) => {
+      //if  the route parameter value  changes  (Observable)
 
-    this.subsign = String(params.get('subsign'))
+      this.subsign = String(params.get('subsign'))
 
-    if(this.subsign=='login'){
-      this.loginBool=true
-      this.signbool=false
-      this.Signn ='Log In'
+      if (this.subsign == 'login') {
+        this.loginBool = true
+        this.signbool = false
+        this.Signn = 'Log In'
+      } else {
+        this.loginBool = false
+        this.signbool = true
+        this.Signn = 'Sign Up'
       }
-      else 
-      {
-        this.loginBool=false
-        this.signbool=true
-        this.Signn ='Sign Up'
-        }
-
-  })
-
-
+    },
+  )
 }
-
--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,62 +1,62 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Pipe, PipeTransform } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { LanguageServiceService } from 'src/app/services/language-service.service';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
+import { Pipe, PipeTransform } from '@angular/core'
+import { AuthService } from 'src/app/services/auth.service'
+import { LanguageServiceService } from 'src/app/services/language-service.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit, OnChanges {
-  public isMenuCollapsed = true;
+  public isMenuCollapsed = true
 
   isuser: boolean = true
 
-// 
+  //
 
-ngOnInit() {
-  if(this.authser.userLogin==true){
-    this.isuser=true
+  ngOnInit() {
+    if (this.authser.userLogin == true) {
+      this.isuser = true
+    } else {
+      this.isuser = false
+    }
+    // this.authser.user.subscribe(user => {
+    //   if (user) {
+    //     this.isuser = false
+    //     // console.log(user)s
+    //   }
+    //   else
+    //     this.isuser = true;
+    // })
+    // if(this.authser.userLogin==true){
+    //   console.log("trueeeee")
+    //   this.isuser=true
+
+    // }
+    // else
+    // {
+    //   console.log("falseee")
+    //   this.isuser=false;
+
+    // }
+    // this.authser.user.subscribe(user => {
+    //   if(user){
+    //   this.isuser=false
+
+    // }
+    //   else
+    //   this.isuser=true;
+    // })
   }
-  else{
-    this.isuser=false
-
-  }
-  // this.authser.user.subscribe(user => {
-  //   if (user) {
-  //     this.isuser = false
-  //     // console.log(user)s
-  //   }
-  //   else
-  //     this.isuser = true;
-  // })
-  // if(this.authser.userLogin==true){
-  //   console.log("trueeeee")
-  //   this.isuser=true
-
-  // }
-  // else
-  // {
-  //   console.log("falseee")
-  //   this.isuser=false;
-
-
-  // }
-  // this.authser.user.subscribe(user => {
-  //   if(user){
-  //   this.isuser=false
-
-  // }
-  //   else
-  //   this.isuser=true;
-  // })
-}
-  constructor(private router:Router,private authser: AuthService, private languageService: LanguageServiceService, private translate: TranslateService
+  constructor(
+    private router: Router,
+    private authser: AuthService,
+    private languageService: LanguageServiceService,
+    private translate: TranslateService,
   ) {
-
     // if (localStorage.getItem("currentUser") === null) {
     //   this.isuser=true
     // }
@@ -64,45 +64,45 @@ ngOnInit() {
     //   this.isuser=false
 
     // }
-    if(this.authser.userLogin==true){
-      this.isuser=true
+    if (this.authser.userLogin == true) {
+      this.isuser = true
+    } else {
+      this.isuser = false
     }
-    else{
-      this.isuser=false
 
-    }
-  
-    
-    this.translate.use(languageService.getLanguage());
+    this.translate.use(languageService.getLanguage())
   }
   ngOnChanges(changes: SimpleChanges): void {
-    if(this.authser.userLogin==true){
-      this.isuser=true
-    }
-    else{
-      this.isuser=false
-  
+    if (this.authser.userLogin == true) {
+      this.isuser = true
+    } else {
+      this.isuser = false
     }
   }
 
   // ngOnInit() {
-  
+
   // }
 
   logout() {
-    localStorage.removeItem('currentUser');
-    localStorage.removeItem('currentUserName');
-console.log(this.authser.userLogin)
-    this.authser.userLogin=true
-console.log(this.authser.userLogin)
+    localStorage.removeItem('currentUser')
+    localStorage.removeItem('currentUserName')
+    console.log(this.authser.userLogin)
+    this.authser.userLogin = true
+    console.log(this.authser.userLogin)
 
-    this.router.navigate(['/']);
+    // this.router.navigate(['/'])
+    // window.history.back()
+    // window.location.reload()
+    this.router.navigate(['/'])
 
+    // this.router.navigate(['/conditions'])
   }
   changeLanguage() {
-    window.location.reload();
-    this.languageService.setLanguage(this.languageService.getLanguage() == 'ar' ? 'en' : 'ar');
-    this.translate.use(this.languageService.getLanguage());
+    window.location.reload()
+    this.languageService.setLanguage(
+      this.languageService.getLanguage() == 'ar' ? 'en' : 'ar',
+    )
+    this.translate.use(this.languageService.getLanguage())
   }
-
 }
